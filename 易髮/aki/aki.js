f3779c8d5ab96bf5a0fa463469a9aa015d791d8d
mokaki <95267354+98672794@users.noreply.github.com>
2022-04-24 01:08:58 +0800

    Add files via upload

---
+++ 易髮/aki/aki.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 202204211552 mokaki.剪髮會員系統 98672794  
+
+
+
+//////////////////////////////
+//////////////////////////////
+/////////// Change ///////////
+//////////////////////////////
+//////////////////////////////
+  // googleTranslate
+    // OVG ALL
+    function googleTranslateElementInit() {
+      new google.translate.TranslateElement({pageLanguage: 'zh-TW'}, 'google_translate_element');  
+    }
+
+
