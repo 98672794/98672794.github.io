082d2a79a899fcc629019e2be4fda7df71eab92d
moksurky <95267354+98672794@users.noreply.github.com>
2022-09-09 08:24:32 +0800

    Add files via upload

--- GreatSage/aki/aki08.js
+++ GreatSage/aki/aki08.js
@@ -1051,8 +1051,8 @@ function _html模板() {
     // https://github.com/davidshimjs/qrcodejs/blob/master/index.html
     function makeCode (Data) {		
       var qrcode = new QRCode(document.getElementById("qrcode"), {
-        width : 400,
-        height : 400,
+        width : 380,
+        height : 380,
         colorDark : "#000",
         colorLight : "#fff",
         correctLevel: QRCode.CorrectLevel.L 
