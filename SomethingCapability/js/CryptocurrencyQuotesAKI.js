fd9a6519849250834ba634b6041948a654fdf2a5
moksurky <95267354+98672794@users.noreply.github.com>
2023-09-03 13:27:19 +0800

    Add files via upload

--- SomethingCapability/js/CryptocurrencyQuotesAKI.js
+++ SomethingCapability/js/CryptocurrencyQuotesAKI.js
@@ -1,4 +1,190 @@
 
+
+
+// Login  MetaMask
+
+
+// STC 代幣餘額 display
+var web3js;
+			  
+					function getERC20TokenBalance(tokenAddress, walletAddress, callback) {
+					  let minABI = [
+						{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"type":"function"},
+						{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"type":"function"}
+					  ];
+					  let contract = new web3js.eth.Contract(minABI, tokenAddress);
+					  contract.methods.balanceOf(walletAddress).call((error, balance) => {
+						contract.methods.decimals().call((error, decimals) => {
+						  console.log(balance);
+						  console.log(decimals);
+						  balance = balance / (10**decimals);
+						  callback(balance);
+						});
+					  });
+					}
+					window.onload = function() {
+					  web3js = new Web3(Web3.givenProvider);
+					  console.log(web3js.version);
+					}
+
+
+
+
+// Login  Ethereum btn
+
+window.userWalletAddress = null
+					const loginButton = document.getElementById('loginButton')
+					const userWallet = document.getElementById('userWallet')
+				
+					function toggleButton() {
+					  if (!window.ethereum) {
+						loginButton.innerText = 'Requires Ethereum supported browser !!!'
+						return false
+					  }
+				
+					  loginButton.addEventListener('click', loginWithMetaMask)
+					}
+				
+					async function loginWithMetaMask() {
+					  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+						.catch((e) => {
+						  console.error(e.message)
+						  return
+						})
+					  if (!accounts) { return }
+				
+					  window.userWalletAddress = accounts[0]
+					  userWallet.innerText = window.userWalletAddress
+					  loginButton.innerText = 'Sign out Ethereum'
+
+					  //<!-- ============ 用STC餘額 ============ -->
+					  var str = $( "#我的錢包2222" ).val(accounts[0]);
+
+					  //<!-- ============ 切割字串 https://www.fooish.com/javascript/string/substr.html ============ -->
+					  $( "#錢包menu" ).html( accounts[0].substr(0, 5) );
+						
+
+					//<!-- ============ STC 代幣餘額 ============ -->
+					let tokenAddress = $( "#STC合約地址" ).val(); //STC合約地址
+					let walletAddress = $( "#我的錢包2222" ).val(); //我的錢All包
+					getERC20TokenBalance(tokenAddress, walletAddress, (balance) => {
+						document.getElementById('STC餘額').innerText = balance.toString();
+					});
+
+					  loginButton.removeEventListener('click', loginWithMetaMask)
+					  setTimeout(() => {
+						loginButton.addEventListener('click', signOutOfMetaMask)
+					  }, 200)
+			  
+					}
+				
+					function signOutOfMetaMask() {
+					  window.userWalletAddress = null
+					  userWallet.innerText = ''
+					  loginButton.innerText = 'Wallet Connect'
+					  //<!-- ============ 用STC餘額 html 靜態 Copy dd Js Aki ============ -->
+					  var str = $( "#我的錢包2222" ).val("0x0"); //val=input
+					  $( "#錢包menu" ).html( 'Wallet Connect');
+                      $( "#STC餘額" ).html( '0.00');
+					  //<!-- ============ / https://jsfiddle.net/mokaki/yu0n2r7z/1/ ============ -->
+			  
+					  loginButton.removeEventListener('click', signOutOfMetaMask)
+					  setTimeout(() => {
+						loginButton.addEventListener('click', loginWithMetaMask)
+					  }, 200)
+					}
+				
+					window.addEventListener('DOMContentLoaded', () => {
+					  toggleButton()
+					});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 當前時間
+// https://cloud.tencent.com/developer/article/2125372
+
+function time() {
+    var vWeek, vWeek_s, vDay;
+    vWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var date =  new Date();
+    year = date.getFullYear();
+    month = date.getMonth() + 1;
+    day = date.getDate();
+    hours = date.getHours();
+    minutes = date.getMinutes();
+    seconds = date.getSeconds();
+    vWeek_s = date.getDay();
+    document.getElementById("time").innerHTML = year + "-" + month + "-" + day + " " + "\t" + hours + ":" + minutes + ":" + seconds + "\t" + vWeek[vWeek_s];
+
+};
+setInterval("time()", 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //  getSTC  Current price<
 
 
@@ -42,13 +228,60 @@ $(document).ready(function() {
 
 
 
-// 發送以太幣
 
-async function getFunds() {
-    const accounts = await ethereum.request({ method: 'eth_accounts' });
-    console.log(accounts[0]);
-    let amount = await document.getElementById("amount").value;
-    amount = amount * 10**18;
-    console.log(amount);
-    const getFunds = await window.contract.methods.getFunds().send({value: amount, from: accounts[0]});
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const infuraUrl = 'https://mainnet.infura.io/v3/5b51d755c35d4ac1a4b771c65ad05a38';
+//console.log()
+//alert()
