e596453e0f08e7b1943d2fb40b7d5a379ddd7d87
moksurky <95267354+98672794@users.noreply.github.com>
2023-12-30 22:41:49 +0800

    Add files via upload

--- 點餐王/aki/aki.js
+++ 點餐王/aki/aki.js
@@ -7,32 +7,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /**
  * 用法:
  * 
@@ -73,32 +47,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* **********************************************************************
 *************************************************************************
 **************************************************** 取Google Sheets 資料 
@@ -122,61 +70,14 @@ var 總Data
 fetch(GEcl).then(res => res.json()).then(res => {
   // 用G資料
   總Data = res
+
+  // page title icon
+  $('title').html(總Data.values[1][0])  //總Data.values[直][橫]
+  $('link[rel="shortcut icon"]').attr('href',總Data.values[1][4])
+
   // 用data總數计加左類Menu // 0是不用
-  for(var 數=0 ; 數 < 總Data.values.length ; 數++){
-    //總Data.values[直][橫]
-    _data入網(數)
-  }
+  for(var 數=0 ; 數 < 總Data.values.length ; 數++){  _data入網(數)  }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -211,12 +112,18 @@ fetch(GEcl).then(res => res.json()).then(res => {
 *********************************************************** 生成all網data 
 *************************************************************************
 *************************************************************************/
+
+
+
+
+
+
+
 function _data入網(數) {
   // get分類名
   let 類名 = 總Data.values[數][0]
 
   _data入網_加入類名menu(類名,數)
-
   _data入網_加入產品(類名,數)  
 
 }
@@ -244,28 +151,13 @@ function _data入網_加入類名menu(類名,數) {
   if (類名.length < 1) return  //類名空pass
 
   // 公司名
-  if (數 === 1){  
-    var 類List = '\
-      <a id="公司名" class="sidebar-brand d-flex align-items-center justify-content-center" href="./">\
-        '+類名+'\
-      </a>\
-      <hr class="sidebar-divider" style="background: rgba(0,0,0,.5);">\
-    '
-  }
+  if (數 === 1){  _data入網_整div('公司名','append','#all類',[類名]) }
+
   // 類名menu
-  if (數 > 2){ //不要exl的說明標題 pass
-    var 類List = '\
-      <!-- '+類名+' -->\
-      <li class="nav-item " title="'+類名+'" >\
-        <a class="類名 nav-link " href="#'+類名+'" >\
-          <span style="font-size: 150%;">'+類名+'</span>\
-          </a>\
-      </li>\
-      <hr class="sidebar-divider" style="background: rgba(0,0,0,.5);">\
-    '// <img src="'+類圖+'" alt="'+類名+'" onerror="this.onerror=null;this.src='+NoIMG+'">\
-  }
+  if (數 > 2){   _data入網_整div('類名menu','append','#all類',[類名])   }
+  //不要exl的說明標題 pass
   
-  $('#all類').append(類List)
+
 }
 
 
@@ -283,41 +175,23 @@ function _data入網_加入類名menu(類名,數) {
 
 
 
-
+var NoIMG = "'aki/NoIMG.jpg'" // monica : How to display a default image if the img tag fails to load an image in JavaScript?
 function _data入網_加入產品(類名,數) {
 
-  let 加購流程 = 總Data.values[數][1]
-  let 類圖 = 總Data.values[數][2]
-  let 品名 = String(總Data.values[數][4]).replace(/\s*/g,"") // 刪空
-  let 產品價錢 = 總Data.values[數][5]
-  let 產品圖 = 總Data.values[數][6]
-  let NoIMG = "'aki/NoIMG.jpg'" // monica : How to display a default image if the img tag fails to load an image in JavaScript?
-  // let dot = "'" // 用於html的 ' 
+  let 加購流程 = 總Data.values[數][1],
+  類圖 = 總Data.values[數][2],
+  品名 = String(總Data.values[數][4]).replace(/\s*/g,""), // 刪空
+  產品價錢 = 總Data.values[數][5],
+  產品圖 = 總Data.values[數][6]
 
   if (數 <= 2) return  //不要exl的說明標題 pass
 
   // 加入產品書籤
-  if (類名.length > 0){ //類名空不顯
-    var 類書籤 = '\
-      <hr id="'+類名+'" style="clear:both; width: 100%;background: #4E73DF;">\
-      <!-- '+類名+'書籤 -->\
-      <h1 class="h3 mb-0 text-gray-800">'+類名+'</h1>\
-      <hr id="_flow_'+加購流程+'_'+類名+'" style="clear:both; width: 100%;opacity: 0;">\
-      '
-    $('#all產品').append(類書籤)
-  }
-
+  if (類名.length > 0){ _data入網_整div('類書籤','append','#all產品',[類名,加購流程])  } //類名空不顯
+  
   // 加入每產品
-  var 餐廳每產品 = '\
-    <button class="產品鍵" onclick="買野(1,'+數 +')">\
-    <img src="'+ 產品圖 +'" alt="'+品名+'" onerror="this.onerror=null;this.src='+NoIMG+'">\
-      <h5>'+品名+'</h5>\
-      <p>$ '+ 產品價錢 +'</p>\
-    </button>\
-  '
+  if (選版 == '餐廳'){   _data入網_整div('餐廳每產品','append','#all產品',[數,產品圖,品名,產品價錢])   }
 
-  if (選版 == '餐廳'){ 每產品 = 餐廳每產品}
-  $('#all產品').append(每產品)
 
 }
 
@@ -339,6 +213,65 @@ function _data入網_加入產品(類名,數) {
 
 
 
+
+
+
+
+
+
+
+function _data入網_整div(sel,run,box_name,data) {
+
+  let 公司名 = '\
+    <a id="公司名" class="sidebar-brand d-flex align-items-center justify-content-center" href="./">\
+    '+data[0]+'\
+    </a>\
+    <hr class="sidebar-divider" style="background: rgba(0,0,0,.5);">\
+  '
+
+  let 類名menu = '\
+    <!-- '+data[0]+' -->\
+    <li class="nav-item " title="'+data[0]+'" >\
+      <a class="類名 nav-link " href="#'+data[0]+'" >\
+        <span style="font-size: 150%;">'+data[0]+'</span>\
+        </a>\
+    </li>\
+    <hr class="sidebar-divider" style="background: rgba(0,0,0,.5);">\
+  '// <img src="'+類圖+'" alt="'+類名+'" onerror="this.onerror=null;this.src='+NoIMG+'">\
+
+  let 類書籤 = '\
+    <hr id="'+data[0]+'" style="clear:both; width: 100%;background: #4E73DF;">\
+    <!-- '+data[0]+'書籤 -->\
+    <h1 class="h3 mb-0 text-gray-800">'+data[0]+'</h1>\
+    <hr id="_flow_'+data[1]+'_'+data[0]+'" style="clear:both; width: 100%;opacity: 0;">\
+  '
+
+  let 餐廳每產品 = '\
+    <button class="產品鍵" onclick="買野(1,'+data[0]+')">\
+    <img src="'+data[1]+'" alt="'+data[2]+'" onerror="this.onerror=null;this.src='+NoIMG+'">\
+      <h5>'+data[2]+'</h5>\
+      <p>$ '+data[3]+'</p>\
+    </button>\
+  '
+
+  let 選項頁 = '\
+    <!-- 產品資料 -->\
+    <div class="card shadow mb-4">\
+        <!-- Card Header - Dropdown -->\
+        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">\
+            <h4 class="m-0 font-weight-bold text-primary">'+data[0]+'</h4><samp >$ '+data[1]+'</samp>\
+        </div>\
+        <!-- Card Body -->\
+        <div class="card-body">\
+          <img src="'+data[2]+'" style="height: auto;width: 100%;max-width: 300px;display: block;margin: auto;" alt="'+data[0]+'" onerror="this.onerror=null;this.src='+NoIMG+'">\
+        </div>\
+    </div>\
+  '
+  // 執行儲存為字串的 JavaScript 程式碼 https://stackoverflow.com/a/939343
+  // append = [run] https://chateverywhere.app/
+  $(box_name)[run](eval(sel))
+
+}
 
 
 
@@ -373,17 +306,20 @@ function _data入網_加入產品(類名,數) {
 
 
 
+
+
+
 // 買野btn動作
 // QQQQ 已選購LIST[].append
 function 買野(動,id) {
 
   // 選版
   if (選版 == '餐廳') _買野_餐廳版(id)
-  //if (選版 == '火鍋') _買野_火鍋版(動,id)
+  if (選版 == '火鍋') _買野_火鍋版(動,id)
 
   // 購物車顯已點產品數
   let 原已點 = $('#已點產品數').text() // 取原價
-  let 新已點 = 原已點
+  ,新已點 = 原已點
   新已點 = ~~新已點+1 // ~~ = str轉int
   $('#已點產品數').text(新已點)
   //console.log('已點產品數=',總新)
@@ -416,53 +352,46 @@ function 買野(動,id) {
 
 
 
-  // _買野_餐廳版
+// _買野_餐廳版
+function _買野_餐廳版(id) {
 
-  function _買野_餐廳版(id) {
+  let 品名 = String(總Data.values[id][4]).replace(/\s*/g,"") // 刪空
+  ,加購流程 = ~~總Data.values[id][1]+0 // 轉數字
+  ,產品價錢 = 總Data.values[id][5]
+  ,可選項 = 總Data.values[id][7]
+  console.log('品名=',品名,'價錢=',產品價錢)
 
-    let 品名 = String(總Data.values[id][4]).replace(/\s*/g,"") // 刪空
-    let 加購流程 = ~~總Data.values[id][1]+0 // 轉數字
-    let 產品價錢 = 總Data.values[id][5]
-    console.log('品名=',品名,'價錢=',產品價錢)
+  //如有可選項
+  if (!!可選項) _買野_產品選項(品名,id) 
+  // 如果想判断一个值不是undefined、null和空字符串中的一种，则用 !! 就可以 
+  https://blog.csdn.net/m0_38039437/article/details/127791259
 
-    // 產品選項
+  /* **********************
+  在GExl用1234...做加購流程
+  如選購的產品有加流程
+  流程號+1檢查網內有沒下一流程的ID
+  如有彈出加購流程
+  ********************** */
+  // if GExl有加流程
+  if ( $(加購流程).length === 0 ) return 
+  // https://jessie75919.medium.com/儘早-return-函數-讓-if-邏輯條件判斷不再一層又一層
 
+  // if有下個流程
+  下個流程 = ~~加購流程+1
+  if ( $('hr[id^="_flow_'+下個流程+'_"]').length === 0 ) return
+  // jQuery判断元素存在与否 https://www.cnblogs.com/asplover/p/14470731.html
+  // jq id模糊查询 https://blog.csdn.net/qq_24909089/article/details/100026008
 
+  let 流程ID= $('hr[id^="_flow_'+下個流程+'_"]').attr('id')
+  // jQuery获取div的id值 https://blog.csdn.net/vancyf/article/details/77881830
+  ,流程ID2 = 流程ID.slice(8)
+  // js 截取字符串 https://www.runoob.com/w3cnote/js-extract-string.html
+  //console.log('下個流程==',流程ID2)
 
-
-
-
-
-
-
-    /* **********************
-    在GExl用1234...做加購流程
-    如選購的產品有加流程
-    流程號+1檢查網內有沒下一流程的ID
-    如有彈出加購流程
-    ********************** */
-    // if GExl有加流程
-    if ( $(加購流程).length === 0 ) return 
-    // https://jessie75919.medium.com/儘早-return-函數-讓-if-邏輯條件判斷不再一層又一層
-
-    // if有下個流程
-    下個流程 = ~~加購流程+1
-    if ( $('hr[id^="_flow_'+下個流程+'_"]').length === 0 ) return
-    // jQuery判断元素存在与否 https://www.cnblogs.com/asplover/p/14470731.html
-    // jq id模糊查询 https://blog.csdn.net/qq_24909089/article/details/100026008
-
-    let 流程ID= $('hr[id^="_flow_'+下個流程+'_"]').attr('id')
-    // jQuery获取div的id值 https://blog.csdn.net/vancyf/article/details/77881830
-    let 流程ID2 = 流程ID.slice(8)
-    // js 截取字符串 https://www.runoob.com/w3cnote/js-extract-string.html
-    //console.log('下個流程==',流程ID2)
-
-    // 轉 購買流程 內容
-    $('#購買流程 h2').text('請問您需要加購'+流程ID2+'嗎?')
-    $('#購買流程 a').eq(0).text('加購'+流程ID2).attr("href",'#'+流程ID2)
-    開關購買流程() // 彈出加購流程
-        
-        
+  // 轉 購買流程 內容
+  $('#購買流程 h2').text('請問您需要加購'+流程ID2+'嗎?')
+  $('#購買流程 a').eq(0).text('加購'+流程ID2).attr("href",'#'+流程ID2)
+  開關購買流程() // 彈出加購流程
 
   }
 
@@ -492,10 +421,116 @@ function 買野(動,id) {
 
 
 
+// 產品選項
+function _買野_產品選項(品名,id) {
+
+  console.log('可選項',總Data.values[id][7])
+
+  _data入網_整div('選項頁','html','#購買流程 .row',[品名,總Data.values[id][5],總Data.values[id][6]]) 
+
+  // 循环取止沒產品選項
+  var 唉 = // 選項名號
+  沒 = 7,
+  
+  //選價 = 唉+1
+  止 = 0
+  
+  do {
+
+    if (!總Data.values[id][唉])  止+=1 // 3次 undefined out
+    else {
+      let 選項價 = 總Data.values[id][唉+1],
+      選項價samp = ''
+      if (!!選項價) 選項價samp = '<samp >$'+選項價+'</samp>'
+
+      let 選項表 = '\
+      <!-- 選項 -->\
+      <div class="card shadow mb-4">\
+        <div class="card-body">\
+          <input type="radio" name="gender" id="Sel_'+唉+'_">\
+            <label for="male">\
+              <span class="text">'+總Data.values[id][唉]+'</span>'+選項價samp+'\
+            </label>\
+          <input type="radio" name="gender" id="female">\
+            <label for="female">\
+              <span class="text">\女</span>\
+            </label>\
+        </div>\
+      </div>\
+      '
+      $('#購買流程 .row').append(選項表)
+    }
+    唉+=2  
+  } while (止<4)
+
+  沒=唉-8
+  /*
+  var 選項表 = '\
+  <!-- 選項 -->\
+    <div class="card shadow mb-4">\
+        <div class="card-body">\
+            <input type="radio" name="gender" id="male">\
+            <label for="male">\
+                <span class="text">\男</span>\
+            </label>\
+            <input type="radio" name="gender" id="female">\
+            <label for="female">\
+                <span class="text">\女</span>\
+            </label>\
+        </div>\
+  </div>\
+  '
+  $('#購買流程 .row').append(選項表)
+  */
+
+
+  var 選項頁 = '\
+  <a href="#" onclick="開關購買流程(0)" class="btn btn-primary btn-block btn-lg">確認</a>\
+  <a onclick="開關購買流程(0)" class="btn btn-block btn-lg">取消</a>\
+  '
+  $('#購買流程 .row').append(選項頁)
 
 
 
-  function _買野_火鍋版(動,id) {
+  開關購買流程() // 彈出產品選項
+
+
+  
+  /*
+  console.log('產品選項END')
+
+  var 選項頁 = '\
+  <h2 class="m-0 font-weight-bold text-primary">嗎?</h2>\
+  <a href="#" onclick="開關購買流程(0)" class="btn btn-primary btn-block btn-lg">加購</a>\
+  <a onclick="確定訂單()" class="btn btn-block btn-lg">no了</a>\
+  '
+  $('#購買流程 .row').html(選項頁)
+  */
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function _買野_火鍋版(動,id) {
   /*
     if (選版 == '火鍋'){ // 火鍋版
       // qqqqqqqqqqq 火鍋未完成 qqqqqqqqqqq
@@ -533,7 +568,7 @@ function 買野(動,id) {
       // qqqqqqqqqqq 火鍋未完成 qqqqqqqqqqq
     }
   */
-  }
+}
 
 
 
