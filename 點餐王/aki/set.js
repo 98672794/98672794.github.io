97a7437bce5fbac3eae413ab11955970f22655d2
moksurky <95267354+98672794@users.noreply.github.com>
2024-02-11 14:43:44 +0800

    Add files via upload

--- 點餐王/aki/set.js
+++ 點餐王/aki/set.js
@@ -15,11 +15,41 @@ function _data入網_整div(sel,run,box_name,data) {
   if (MOK) console.log("_data入網_整div('類名menu','append','#all類',[類名])")
 
   // 網頁data
-  let 公司名 = '\
-    <a id="公司名" style="color: '+網字色1號+'" class="sidebar-brand d-flex align-items-center justify-content-center" href="'+data[1]+'">\
-    '+data[0]+'\
-    </a>\
-    <hr>\
+  let LoginPage = '\
+  <div class="row justify-content-center"><div class="col-xl-10 col-lg-12 col-md-9"><div class="row"><div class="col-lg-12"><div class="p-5">\
+    <div class="text-center">\
+        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>\
+    </div>\
+    <form class="user">\
+        <div class="form-group">\
+            <input type="ID" class="form-control form-control-user"\
+                id="UserID" placeholder="Enter ID">\
+        </div>\
+        <div class="form-group">\
+            <input type="password" class="form-control form-control-user"\
+                id="UserPW" placeholder="Password">\
+        </div>\
+        <div class="form-group">\
+            <div class="custom-control custom-checkbox small">\
+                <input type="checkbox" class="custom-control-input" id="customCheck">\
+                <label class="custom-control-label" for="customCheck">\
+                    Remember Me\
+                </label>\
+            </div>\
+        </div>\
+        <a onclick="Login()" class="btn btn-primary btn-user btn-block">\
+            Login\
+        </a>\
+        <hr>\
+        <div class="text-center">\
+        <a href="Create會員.html" class=" btn-block">Create an Account!</a>\
+        </div>\
+        <hr>\
+        <div class="text-center">\
+            <a class="small " href="https://wa.me/">Forgot Password?</a>\
+        </div>\
+    </form>\
+  </div></div></div></div></div>\
   '
   $(box_name)[run]([eval(sel)])
 
@@ -32,4 +62,4 @@ function _data入網_整div(sel,run,box_name,data) {
 
 
 
-_data入網_整div('公司名','append','#all類',[類名,新入網Ulr做主頁]) 
+_data入網_整div('LoginPage','append','#set_page','1') 
