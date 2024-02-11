594991a8aa253b6c9e54aea04cc7f6679e59ed08
moksurky <95267354+98672794@users.noreply.github.com>
2024-02-11 14:03:37 +0800

    Add files via upload

---
+++ 點餐王/aki/set.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+var dot = "'"
+function _data入網_整div(sel,run,box_name,data) {
+  if (MOK) console.log("_data入網_整div('類名menu','append','#all類',[類名])")
+
+  // 網頁data
+  let 公司名 = '\
+    <a id="公司名" style="color: '+網字色1號+'" class="sidebar-brand d-flex align-items-center justify-content-center" href="'+data[1]+'">\
+    '+data[0]+'\
+    </a>\
+    <hr>\
+  '
+  $(box_name)[run]([eval(sel)])
+
+}
+
+
+
+
+
+
+
+
+_data入網_整div('公司名','append','#all類',[類名,新入網Ulr做主頁]) 
