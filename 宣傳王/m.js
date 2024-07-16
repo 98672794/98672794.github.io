1482b0904d17f36977fbb08f55eb9b512ae6c2ac
moksurky <95267354+98672794@users.noreply.github.com>
2024-07-17 00:40:36 +0800

    Add files via upload

--- 宣傳王/m.js
+++ 宣傳王/m.js
@@ -9,10 +9,10 @@ function _網插件(sel,run,box_name,data) {
   '
   let 導航 = '\
     <ul>\
-      <li><a href="/">首頁</a></li>\
-      <li><a href="/MYad.html">我的廣告</a></li>\
-      <li><a href="/about.html">關於我們</a></li>\
-      <li><a href="/contact.html">聯絡我們</a></li>\
+      <li><a href="./">首頁</a></li>\
+      <li><a href="./MYad.html">我的廣告</a></li>\
+      <li><a href="./about.html">關於我們</a></li>\
+      <li><a href="./contact.html">聯絡我們</a></li>\
     </ul>\
     \
   '
